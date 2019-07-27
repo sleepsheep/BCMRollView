7fbd1ca53d2bd08b49ef59420a066186a7088fcc
Kevin <liuyang_bz@163.com>
2019-07-27 20:23:42 +0800

    Update BCMRollView.h

--- BCMRollView/BCMRollView.h
+++ BCMRollView/BCMRollView.h
@@ -5,6 +5,24 @@
 //  Created by Kevin on 2019/7/27.
 //  Copyright © 2019 LY. All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
 
 /** 52,000.00
  1、数字从0开始滚动到9，数字滚动不要有跳跃性，比如第一个数字5滚动一圈为0->5，第二个数字2滚动两圈为0->9-0->2,第三个数字0滚动三圈为0-9-0-9-0 以此类推
